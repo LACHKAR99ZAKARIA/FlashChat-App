ef860936670ff69ce01b974fca1e67fd51e1bf55
LACHKAR Zakaria <97468839+LACHKAR99ZAKARIA@users.noreply.github.com>
2023-03-28 10:55:34 +0000

    Update RegisterViewController.swift

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    let auth = Auth.auth()
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text , let password = passwordTextfield.text{
+            auth.createUser(withEmail: email, password: password) { authResult, error in
+              // Vérifiez si l'opération de création d'utilisateur a réussi ou a échoué
+              if let error = error {
+                // Une erreur s'est produite, affichez un message d'erreur à l'utilisateur
+                print("Erreur de création d'utilisateur: \(error.localizedDescription)")
+              } else {
+                print("L'utilisateur a été créé avec succès!")
+                  self.performSegue(withIdentifier: K.registerSegue, sender: self)
+              }
+            }
+        }
     }
     
 }
