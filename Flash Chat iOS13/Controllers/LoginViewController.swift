3b4864d03cb5870043ace02fd5714a6edb0e933b
LACHKAR Zakaria <97468839+LACHKAR99ZAKARIA@users.noreply.github.com>
2023-03-28 10:55:20 +0000

    Update LoginViewController.swift

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    let auth = Auth.auth()
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text , let password = passwordTextfield.text{
+            auth.signIn(withEmail: email, password: password) { authResult, error in
+              // Vérifiez si l'opération de connexion a réussi ou a échoué
+              if let error = error {
+                // Une erreur s'est produite, affichez un message d'erreur à l'utilisateur
+                print("Erreur de connexion: \(error.localizedDescription)")
+              } else {
+                // L'opération de connexion a réussi, vous pouvez maintenant utiliser l'objet authResult pour accéder aux informations sur l'utilisateur connecté
+                print("L'utilisateur a été connecté avec succès!")
+                self.performSegue(withIdentifier: K.loginSegue, sender: self)
+              }
+            }
+        }
     }
     
 }
